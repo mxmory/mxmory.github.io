868dca71339915e43629c89f3542176a8f6178bf
Khisa <vydokh@gmail.com>
2020-09-16 20:38:14 +0500

    Updates

---
+++ precache-manifest.92f0ea7484be50975eff1619ccc67513.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb17a95344a42180bb012fb1996f3f97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a4601028aa3e950ad04",
+    "url": "/static/css/main.c9899ba2.chunk.css"
+  },
+  {
+    "revision": "e4c0d94d6301c68f9ba6",
+    "url": "/static/js/2.fe2c83b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fe2c83b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a4601028aa3e950ad04",
+    "url": "/static/js/main.fb1d2f7e.chunk.js"
+  },
+  {
+    "revision": "c39aee8082bbc7e6526c",
+    "url": "/static/js/runtime-main.a98211ce.js"
+  },
+  {
+    "revision": "0dbdefe6847fe9016dd44aedfbe9aeac",
+    "url": "/static/media/sprite2.0dbdefe6.gif"
+  }
+]);
